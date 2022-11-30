0776327193374446e4a1181a61c3a6e1c8028da1
IBKBrako <brightkonadubrako@gmail.com>
2022-11-29 21:10:38 -0800

    Task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -16,4 +16,4 @@ def fizzbuzz():
         elif number % 5 == 0:
             print(" Buzz", end="")
         else:
-            print("{} ".format(number), end="")
+            print(" {}".format(number), end="")
